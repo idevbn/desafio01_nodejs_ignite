c54115a69761bbfd66d98e3574273fb6495fc4c5
Idevaldo <inetomec@yahoo.com.br>
2021-12-09 00:22:49 -0300

    feat: :sparkles: Finishing Challenge

--- src/__tests__/users.spec.js
+++ src/__tests__/users.spec.js
@@ -1,43 +1,43 @@
-const request = require('supertest');
-const { validate } = require('uuid');
+const request = require("supertest")
+const { validate } = require("uuid")
 
-const app = require('../');
+const app = require("../")
 
-describe('Users', () => {
-  it('should be able to create a new user', async () => {
+describe("Users", () => {
+  it("should be able to create a new user", async () => {
     const response = await request(app)
-      .post('/users')
+      .post("/users")
       .send({
-        name: 'John Doe',
-        username: 'johndoe'
+        name: "John Doe",
+        username: "johndoe"
       })
-    expect(201);
+    expect(201)
 
-    expect(validate(response.body.id)).toBe(true);
+    expect(validate(response.body.id)).toBe(true)
 
     expect(response.body).toMatchObject({
-      name: 'John Doe',
-      username: 'johndoe',
+      name: "John Doe",
+      username: "johndoe",
       todos: []
-    });
-  });
+    })
+  })
 
-  it('should not be able to create a new user when username already exists', async () => {
+  it("should not be able to create a new user when username already exists", async () => {
     await request(app)
-      .post('/users')
+      .post("/users")
       .send({
-        name: 'John Doe',
-        username: 'johndoe'
-      });
+        name: "John Doe",
+        username: "johndoe"
+      })
 
     const response = await request(app)
-      .post('/users')
+      .post("/users")
       .send({
-        name: 'John Doe',
-        username: 'johndoe'
+        name: "John Doe",
+        username: "johndoe"
       })
-      .expect(400);
+      .expect(400)
 
-    expect(response.body.error).toBeTruthy();
-  });
-});
+    expect(response.body.error).toBeTruthy()
+  })
+})
